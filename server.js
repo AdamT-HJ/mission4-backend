f763b681a507b096e29287146b31326fb80f2ab4
AdamT <adamt@missionreadyhq.com>
2025-06-09 15:06:44 +1200

    2nd push, session management added, API endpoint to be written

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import axios from "axios";
 // import {GoogleGenAi} from "@google/genai";
 import fs from "fs/promises";
 import path from "path";
+import { get } from "https";
 
 //Instantiate Express app and define port
 const app = express();
@@ -26,9 +27,65 @@ app.use(cors({
 //===VARIABLES=======//
 const apiKey = process.env.API_KEY;
 
+const sessionsDirectory = process.env.SESSIONS_DIR || "./sessions";
+
 
 
 //====FILE MANAGEMENT FOR "SESSIONS"====//
+// create directory or do nothing if already exists 
+async function createOrCheckForSessionsDirectory(){
+    try{
+        await fs.mkdir(sessionsDirectory, {recursive: true});
+        console.log(`Sessions Directory created or existing at:${sessionsDirectory}`);
+    }
+    catch (error) {
+        console.error(`error in creating or confirming sessions directory exists:${error.message}`);
+    }
+};
+
+//Create path for new/existing sessions 
+// takes sessionId and adds to dir. path with .json extn. to create correct file path.
+function getSessionFilePath(sessionId){
+    return path.join(sessionsDirectory, `${sessionId}.json`);
+}
+
+//To save session data to a file
+async function saveSession(sessionId, sessionData) {
+    const filePath = getSessionFilePath(sessionId);
+    try{
+        await fetch.writeFile(filePath, JSON.stringify(sessionData, null, 2), "utf8");
+        console.log(`Session ${sessionId} saved.`);
+    }
+        //re-throw new error to signal failure to calling function
+        // ensures promise returned by saveSession rejects, otherwise may appear to have executed successfully because no error passed back.
+    catch (error) {
+        console.error(`Error saving ${sessionid}:`, error);
+        throw new Error (`Failed to save session ${sessionId}: ${error.message}`);
+    }
+};
+
+// To load session data from a file
+// uses getSessionFilePath, 
+async function loadSession(sessionId) {
+    const filePath = getSessionFilePath(sessionId);
+    try{
+        const data = await fs.readFile(filePath, "utf8");
+        return JSON.parse(data);
+    }
+    catch (error) {
+        //ENOENT - Error no Entry
+        if (error.code === "ENOENT"){
+            return null;
+        }
+        console.error(`Error loading session ${sessionId}:`, error);
+        throw new Error(`Failed to load session ${sessionId}`);
+    }
+};
+
+//To Generate session ID
+function generateSessionId(){
+    return Math.random().toString(36).substring(2,18);
+}
 
 
 
@@ -39,6 +96,52 @@ const apiKey = process.env.API_KEY;
 app.get("/test", (req, res) => {
     res.status(200).json({message: "test successful from node.js!"});
 });
+
+
+//Session Management Endpoint 
+app.get("/session", async (req, res)=>{
+    const requestedSessionId = req.query.sessionId;
+
+    try{
+        let sessionId;
+        let sessionData;
+
+        //trying to load existing session on request
+        if(requestedSessionId) {
+            const loadedSession = await loadSession(requestedSessionId);
+            if(loadedSession){
+                sessionId = requestedSessionId;
+                sessionData = loadedSession;
+                return res.json({
+                    sessionId,
+                    conversationHistory: sessionData.conversationHistory
+                });
+            }
+            else {
+                return res.status(400).json({message: "session ID not found"});
+            }
+        }
+        else {
+            //create new session if no requested sessionId
+            sessionId = generateSessionId();
+            sessionData = {
+                conversationHistory:[]
+            };
+            //save session
+            await saveSession(sessionId, sessionData);
+            return res.status(200).json({sessionId, conversationHistory:[]})
+        
+            //insert code to do initial call to Gemini to get greeting to user.
+            
+        }
+    }
+    catch (error) {
+        console.error("Error in /session endpoint", error);
+        res.status(500).json({message:"Internal server error in session management, in loading or creating new session."})
+    }
+});
+
+//Chat Endpoint for Gemini (LLM) AND response to frontend
 
 
 
